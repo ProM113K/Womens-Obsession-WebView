e064821bd2569382c9c08df8ac6c2e0d47a3710c
Dimas Putra Widiyanto <putrad322@gmail.com>
2023-02-02 16:17:13 +0700

    Change icon apps

--- app/src/main/java/com/obsessionmediagroup/womensobsession/MainActivity.kt
+++ app/src/main/java/com/obsessionmediagroup/womensobsession/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.obsessionmediagroup.womensobsession
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.core.view.WindowInsetsCompat
 
+@SuppressLint("SetJavaScriptEnabled")
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.wo_webView)
+
+        WindowInsetsCompat.Type.navigationBars()
+
+        webView.webViewClient = WebViewClient()
+
+        webView.loadUrl(WEB_URL)
+
+        webView.settings.javaScriptEnabled = true
+
+        webView.settings.setSupportZoom(true)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    companion object {
+        private const val WEB_URL = "https://www.womensobsession.com/"
     }
 }
